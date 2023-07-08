8131d1f590e0f00557269a76a4624926359ff21a
xyzeque <xyzequel@gmail.com>
2023-07-08 10:50:05 +0800

    Add playRound function for game logic

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
+// get computer choice using Math.random
 function getComputerChoice() {
   const choice = Math.floor(Math.random() * 3) + 1;
-  alert(choice);
   console.log(choice);
   switch (choice) {
     case 1:
@@ -17,3 +17,26 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
+// rock paper scissors game logic
+function playRound(playerSelection, computerSelection) {
+
+  if (playerSelection == computerSelection) {
+    return "It's a Tie!"
+  } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    return "You Win! Rock beats Paper";
+  } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    return "You Win! Scissors beats Paper";
+  }
+}
+
+const computerSelection = getComputerChoice();
+console.log(playRound('scissors', computerSelection));
